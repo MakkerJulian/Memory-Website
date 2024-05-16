dc239ac07581088bb37b73b5c3df0bba1ca223d9
MakkerJulian <juliankloosterhuis@gmail.com>
2024-05-16 14:50:09 +0200

    week 3

--- memory.js
+++ memory.js
@@ -1,78 +1,117 @@
 var savedCard = null;
-var foundIds = [];
-function changeOpen() {
-  var openInput = document.getElementById("openInput");
-  var openCards = document.getElementsByClassName("open");
-  console.log(openCards);
+var savedCard1 = null;
+var savedCard2 = null;
+var matched = 0;
+var globalSize = 18;
 
-  for (var i = 0; i < openCards.length; i++) {
-    openCards[i].style.backgroundColor = openInput.value;
-  }
+function changeOpen() {
+	var openInput = document.getElementById("openInput");
+	var openCards = document.getElementsByClassName("open");
+
+	for (var i = 0; i < openCards.length; i++) {
+		openCards[i].style.backgroundColor = openInput.value;
+	}
 }
 
 function changeMatch() {
-  var matchInput = document.getElementById("matchedInput");
-  var matchCards = document.getElementsByClassName("match");
+	var matchInput = document.getElementById("matchedInput");
+	var matchCards = document.getElementsByClassName("match");
 
-  for (var i = 0; i < matchCards.length; i++) {
-    matchCards[i].style.backgroundColor = matchInput.value;
-  }
+	for (var i = 0; i < matchCards.length; i++) {
+		matchCards[i].style.backgroundColor = matchInput.value;
+	}
 }
 
 function changeClose() {
-  var closeInput = document.getElementById("closedInput");
-  var closeCards = document.getElementsByClassName("close");
+	var closeInput = document.getElementById("closedInput");
+	var closeCards = document.getElementsByClassName("close");
 
-  for (var i = 0; i < closeCards.length; i++) {
-    closeCards[i].style.backgroundColor = closeInput.value;
-  }
+	for (var i = 0; i < closeCards.length; i++) {
+		closeCards[i].style.backgroundColor = closeInput.value;
+	}
 }
 
-function generateCards(size = 18) {
-  //generate a random list of letters
-  var letters = [];
-  for (let i = 0; i < size; i++) {
-    letters.push(String.fromCharCode(65 + i));
-    letters.push(String.fromCharCode(65 + i));
-  }
-  letters.sort(() => Math.random() - 0.5);
+function reset() {
+	matched = 0;
+	savedCard = null;
+	savedCard1 = null;
+	savedCard2 = null;
+	changeScore(0);
+}
 
-  var cardContainer = document.getElementById("cardContainer");
-  //empty the card container
-  cardContainer.innerHTML = "";
-  for (let i = 0; i < size * 2; i++) {
-    var card = document.createElement("button");
-    card.className = "card close";
-    card.id = "card"+i;
-    card.value = letters[i];
-    card.onclick = () => changeToOpen(i);
-    cardContainer.appendChild(card);
-  }
+function changeSize(size) {
+	globalSize = size;
+	generateCards();
+}
+
+function changeScore(score){
+	document.getElementById('score').innerHTML= "Score: "+ score;
+}
+
+function generateLetters() {
+	//generate a random list of letters
+	var letters = [];
+	for (let i = 0; i < globalSize; i++) {
+		letters.push(String.fromCharCode(65 + i));
+		letters.push(String.fromCharCode(65 + i));
+	}
+	letters.sort(() => Math.random() - 0.5);
+	return letters;
+}
+
+function generateCards() {
+	reset();
+
+	var letters = generateLetters();
+	var cardContainer = document.getElementById("cardContainer");
+	//empty the card container
+	cardContainer.innerHTML = "";
+	for (let i = 1; i < (globalSize * 2) + 1; i++) {
+		var card = document.createElement("button");
+		card.className = "card close";
+		card.id = "card" + i;
+		card.value = letters[i - 1];
+		card.onclick = () => changeToOpen(i);
+		cardContainer.appendChild(card);
+	}
 }
 
 function changeToOpen(cardId) {
-  var card = document.getElementById("card"+cardId);
-  card.className = "card open";
-  card.innerHTML = card.value;
-  if(foundIds.includes(cardId)){
-    return;
-  }
-  if (!savedCard) {
-    savedCard = cardId;
-  } else {
-    var card2 = document.getElementById("card"+savedCard);
-    if (card2.value === card.value) {
-      card.className = "card match";
-      card2.className = "card match";
-      savedCard = null;
-    } else {
-      setTimeout(() => {
-        card.className = "card close";
-        card2.className = "card close";
-        savedCard = null;
-        card.innerHTML = "";
-        card2.innerHTML = "";
-      }, 500);
-    }
-  }
+	var card = document.getElementById("card" + cardId);
+
+	if (card.className === "card match" || savedCard === cardId) return;
+
+	card.className = "card open";
+	card.innerHTML = card.value;
+
+	if (!savedCard) {
+		var card1 = document.getElementById("card" + savedCard1);
+		var card2 = document.getElementById("card" + savedCard2);
+		if (card1 && card2 && card1.className != "card match") {
+			card1.className = "card close";
+			card2.className = "card close";
+			card1.innerHTML = "";
+			card2.innerHTML = "";
+		}
+		savedCard = cardId;
+		savedCard1 = cardId;
+	} else {
+		savedCard2 = cardId;
+		var card2 = document.getElementById("card" + savedCard);
+		if (card2.value === card.value) {
+			card.className = "card match";
+			card2.className = "card match";
+			savedCard = null;
+			matched++;
+			changeScore(matched);
+			if (matched == globalSize) congratulateAndReset();
+		} else {
+			savedCard = null;
+		}
+	}
+}
+
+function congratulateAndReset() {
+	alert("Congratulations! You have won!");
+	generateCards(globalSize);
 }
